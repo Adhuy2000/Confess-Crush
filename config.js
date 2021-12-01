1ec12c671066b5d75aaf900214470b8072b32c4b
Adhuy2000 <94697474+Adhuy2000@users.noreply.github.com>
2021-12-01 21:42:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'Các Em Có Muốn Làm NY Của Anh Ko,
+    desc: 'Nếu Ko Trả Lời Mà Thoát Ra Tức alaf Muốn Làm NY Của Anh R Đs',
+    btnYes: 'Vâng , Em Muốn Làm NY Của Anh',
+    btnNo: 'Không, Anh Mơ À :))',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh ;))',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Em Thích Anh Lâu Rồi , Em Muốn Làm NY Của Anh ;)) ',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/huytrumsvzxc'
 }
